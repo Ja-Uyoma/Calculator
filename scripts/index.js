d3b1df5e3c8253097dea042852dfbb5690cc1c1a
Jimmy Givans <jauyoma.dev@gmail.com>
2023-11-28 11:53:17 +0300

    Add docs to the what method in IllegalOperationException

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,10 @@
 "use strict";
 
 class IllegalOperationException extends Error {
+    /**
+     * Get the error message describing the exception that occured
+     * @returns {string} An message describing the error that occured
+     */
     what() {
         return this.message;
     }
