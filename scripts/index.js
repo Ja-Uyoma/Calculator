59689887f7f08b1bea32023efb6eb44890c81908
Jimmy Givans <jauyoma.dev@gmail.com>
2023-12-05 10:41:38 +0300

    Delete function operate and the modules it relies on
    - The function is still unused and hence unnecessary for the time
    being

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 "use strict";
 
 import { updateYear } from "./UpdateYear.js";
-import { add, subtract, multiply, divide } from "./MathOperations.js";
 import {
   updateDisplay,
   clearDisplay,
@@ -11,33 +10,5 @@ import {
 updateDisplay();
 clearDisplay();
 deleteCharacter();
-
-/**
- * Determine an operation to perform and execute it
- * @param {string} infixOperator The operation to perform
- * @param {number} first The first argument to the operator
- * @param {number} second The second argument to the operator
- * @returns The result of performing the specified operation
- */
-function operate(infixOperator, first, second) {
-  if (infixOperator === "+") {
-    return add(first, second);
-  } else if (infixOperator === "-") {
-    return subtract(first, second);
-  } else if (infixOperator === "*") {
-    return multiply(first, second);
-  } else if (infixOperator === "/") {
-    let outcome = 1;
-
-    try {
-      outcome = divide(first, second);
-    } catch (err) {
-      console.error(err.what());
-      outcome = null;
-    }
-
-    return outcome;
-  }
-}
 
 updateYear();
