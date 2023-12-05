1cc1233fa564f45d8afa4884c4742a09d7264923
Jimmy Givans <jauyoma.dev@gmail.com>
2023-12-05 10:03:53 +0300

    Move math operation functions to separate module

--- scripts/index.js
+++ scripts/index.js
@@ -1,50 +1,6 @@
 "use strict";
 
-import IllegalOperationException from "./IllegalOperationException";
-
-/**
- * Add two numbers
- * @param {number} first The first number in the operation
- * @param {number} second The second number in the operation
- * @returns The sum of the two numbers
- */
-function add(first, second) {
-  return first + second;
-}
-
-/**
- * Subtract two numbers
- * @param {number} first The first number in the operation
- * @param {number} second The second number in the operation
- * @returns The difference of the two numbers
- */
-function subtract(first, second) {
-  return first - second;
-}
-
-/**
- * Multiply two numbers
- * @param {number} first The first number in the operation
- * @param {number} second The second number in the operation
- * @returns The product of the two numbers
- */
-function multiply(first, second) {
-  return first * second;
-}
-
-/**
- * Divide two numbers
- * @param {number} first The first number in the operation
- * @param {number} second The second number in the operation
- * @returns The quotient of the two numbers
- */
-function divide(first, second) {
-  if (second === 0) {
-    throw new IllegalOperationException("Division by zero is undefined");
-  }
-
-  return first / second;
-}
+import { add, subtract, multiply, divide } from "./MathOperations";
 
 let firstOperand = null;
 let secondOperand = null;
