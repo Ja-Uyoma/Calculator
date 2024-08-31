c58ba18125c41aa06f4ebb4be3694ef38e43559e
Jimmy Givans <jauyoma.dev@gmail.com>
2024-08-31 13:46:56 +0300

    test: add tests for Tokenizer class methods

--- src/Tokenizer.test.ts
+++ src/Tokenizer.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { flushBuffer, tokenizer } from "./Tokenizer";
+import { flushBuffer, Tokenizer, tokenizer } from "./Tokenizer";
 
 describe("flushBuffer", () => {
   it("Is a no-op if the buffer is empty", () => {
@@ -66,5 +66,60 @@ describe("tokenizer", () => {
       "+",
       "10",
     ]);
+  });
+});
+
+describe("Tokenizer", () => {
+  it("hasMoreTokens correctly gauges the presence of tokens in the input string", () => {
+    let tokenizer = new Tokenizer("");
+    expect(tokenizer.hasMoreTokens()).toBe(false);
+
+    tokenizer = new Tokenizer("1 + 2 + 3");
+    expect(tokenizer.hasMoreTokens()).toBe(true);
+  });
+
+  it("match correctly matches against whitespace input", () => {
+    let tokenizer = new Tokenizer("");
+    expect(tokenizer.match(/^\s+/, " ")).toBe(" ");
+  });
+
+  it("match correctly matches against numeric input", () => {
+    let tokenizer = new Tokenizer("");
+    expect(tokenizer.match(/^(?:\d+(:?\.\d*)?|\.\d+)/, "2")).toBe("2");
+    expect(tokenizer.match(/^(?:\d+(:?\.\d*)?|\.\d+)/, "2.2")).toBe("2.2");
+    expect(tokenizer.match(/^(?:\d+(:?\.\d*)?|\.\d+)/, "-2")).toBe(null);
+  });
+
+  it("match correctly matches against operators", () => {
+    let tokenizer = new Tokenizer("");
+
+    expect(tokenizer.match(/^\-/, "-")).toBe("-");
+    expect(tokenizer.match(/^\+/, "+")).toBe("+");
+    expect(tokenizer.match(/^\×/, "×")).toBe("×");
+    expect(tokenizer.match(/^\÷/, "÷")).toBe("÷");
+    expect(tokenizer.match(/^\^/, "^")).toBe("^");
+    expect(tokenizer.match(/^\(/, "(")).toBe("(");
+    expect(tokenizer.match(/^\)/, ")")).toBe(")");
+  });
+
+  it("getNextToken returns null if there are no tokens left", () => {
+    let tokenizer = new Tokenizer("");
+    expect(tokenizer.getNextToken()).toBe(null);
+  });
+
+  it("getNextToken returns the next token in the input expression", () => {
+    let tokenizer = new Tokenizer("1 + 2");
+    expect(tokenizer.getNextToken()).toStrictEqual({
+      type: "NUMBER",
+      value: "1",
+    });
+    expect(tokenizer.getNextToken()).toStrictEqual({
+      type: "+",
+      value: "+",
+    });
+    expect(tokenizer.getNextToken()).toStrictEqual({
+      type: "NUMBER",
+      value: "2",
+    });
   });
 });
